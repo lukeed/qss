338357a1a0489ce99b7fc059498a6a6ef54ad97c
Luke Edwards <luke.edwards05@gmail.com>
2017-10-22 16:15:24 -0700

    add simple tests; compare to native

--- test/index.js
+++ test/index.js
@@ -1,6 +1,53 @@
 const test = require('tape');
-const fn = require('../lib');
+const q = require('querystring');
+const fn = require('../dist/qss');
 
-test('foo', t => {
-	t.pass();
+const native = q.stringify;
+
+test('qss', t => {
+	t.is(typeof fn, 'function', 'exports a function');
+	t.end();
+});
+
+test('simple', t => {
+	let obj = { foo:123 };
+	let out = fn(obj);
+	t.is(out, 'foo=123', 'returns expected value');
+	t.is(out, native(obj), 'matches native value');
+	t.is(typeof out, 'string', 'returns a string');
+	t.end();
+});
+
+test('simple - multiple', t => {
+	let obj = { foo:123, bar:456 };
+	let out = fn(obj);
+	t.is(out, 'foo=123&bar=456', 'returns expected value');
+	t.is(out, native(obj), 'matches native value');
+	t.end();
+});
+
+test('array', t => {
+	let obj = { foo:[1,2,3] };
+	let out = fn(obj);
+	t.is(out, 'foo=1&foo=2&foo=3', 'returns expected value');
+	t.is(out, native(obj), 'matches native value');
+	t.end();
+});
+
+test('array - multiple', t => {
+	let obj = { foo:[1,2,3], bar:[4,5,6] };
+	let out = fn(obj);
+	t.is(out, 'foo=1&foo=2&foo=3&bar=4&bar=5&bar=6', 'returns expected value');
+	t.is(out, native(obj), 'matches native value');
+	t.end();
+});
+
+test('array - nested', t => {
+	let obj = { foo:[ 1,[2],3] };
+	let val = native(obj);
+	let out = fn(obj);
+	t.is(out, 'foo=1&foo=2&foo=3', 'returns expected value');
+	t.is(val, 'foo=1&foo=&foo=3', '>> assert native value, for reference');
+	t.not(out, val, 'does NOT match native value');
+	t.end();
 });
