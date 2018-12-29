a68979a29ed63fab3a8952488f7877ce04c0d774
Luke Edwards <luke.edwards05@gmail.com>
2018-12-28 22:41:44 -0800

    chore: update builder for named exports

--- builder.js
+++ builder.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const mkdir = require('mk-dirs');
+const sizer = require('gzip-size');
 const pretty = require('pretty-bytes');
 const { minify } = require('terser');
-const sizer = require('gzip-size');
 const pkg = require('./package');
 
 let data = fs.readFileSync('src/index.js', 'utf8');
@@ -11,15 +11,31 @@ mkdir('dist').then(() => {
 	// Copy as is for ESM
 	fs.writeFileSync(pkg.module, data);
 
-	// Mutate exports for CJS
-	data = data.replace(/export default/, 'module.exports =');
-	fs.writeFileSync(pkg.main, data);
-
-	// Write UMD bundle
-	let name = pkg.name;
+	// Minify & print gzip-size
 	let { code } = minify(data, { toplevel:true });
 	console.log(`> gzip size: ${pretty(sizer.sync(code))}`);
-	let UMD = `!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):e.${name}=t()}(this,function(){`;
-	UMD += code.replace(/module.exports=/, 'return ') + '});';
-	fs.writeFileSync(pkg.unpkg, UMD);
+
+	let keys = [];
+	// Mutate exports for CJS
+	data = data.replace(/export function\s?(.+?)(?=\()/gi, (_, x) => {
+		return keys.push(x) && `function ${x}`;
+	});
+	keys.sort().forEach(key => {
+		data += `\nexports.${key} = ${key};`;
+	});
+	fs.writeFileSync(pkg.main, data + '\n');
+
+	let name = pkg['umd:name'] || pkg.name;
+
+	// Write UMD bundle
+	let UMD = minify(`
+	(function (global, factory) {
+		typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+		typeof define === 'function' && define.amd ? define(['exports'], factory) :
+		(factory((global['${name}'] = {})));
+	}(this, (function (exports) {
+		${data}
+	})));`);
+
+	fs.writeFileSync(pkg.unpkg, UMD.code);
 });
