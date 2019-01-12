69fc22312519b685254fb44ca430ae6758617c0c
Luke Edwards <luke.edwards05@gmail.com>
2019-01-12 15:53:47 -0800

    chore: add `qs` to benchmarks;
    - Closes #1

--- bench/index.js
+++ bench/index.js
@@ -1,3 +1,4 @@
+const qs = require('qs');
 const native = require('querystring');
 const { Suite } = require('benchmark');
 const queryString = require('query-string');
@@ -12,6 +13,7 @@ const str = 'foo=123&bar=4&bar=5&bar=6&baz=true';
 
 console.log('\nEncode:');
 foo
+	.add('qs             ', () => qs.stringify(obj))
 	.add('querystringify ', () => querystringify.stringify(obj))
 	.add('query-string   ', () => queryString.stringify(obj))
 	.add('native         ', () => native.stringify(obj))
@@ -21,6 +23,7 @@ foo
 
 console.log('\nDecode:');
 bar
+	.add('qs             ', () => qs.parse(str))
 	.add('querystringify ', () => querystringify.parse(str))
 	.add('query-string   ', () => queryString.parse(str))
 	.add('native         ', () => native.parse(str))
